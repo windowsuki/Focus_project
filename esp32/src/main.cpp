7d04c373c152b085eb82378831d34b2c46897401
Wu Yue <98885271+windowsuki@users.noreply.github.com>
2025-01-19 15:43:08 +0800

    esp32 updated

---
+++ esp32/src/main.cpp
@@ -0,0 +1,177 @@
+/*
+  ArduinoMqttClient - WiFi Advanced Callback
+
+  This example connects to a MQTT broker and subscribes to a single topic,
+  it also publishes a message to another topic every 10 seconds.
+  When a message is received it prints the message to the Serial Monitor,
+  it uses the callback functionality of the library.
+
+  It also demonstrates how to set the will message, get/set QoS, 
+  duplicate and retain values of messages.
+
+  The circuit:
+  - Arduino MKR 1000, MKR 1010 or Uno WiFi Rev2 board
+
+  This example code is in the public domain.
+*/
+#include <Arduino.h>
+#include <ArduinoMqttClient.h>
+#if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_AVR_UNO_WIFI_REV2)
+  #include <WiFiNINA.h>
+#elif defined(ARDUINO_SAMD_MKR1000)
+  #include <WiFi101.h>
+#elif defined(ARDUINO_ARCH_ESP8266)
+  #include <ESP8266WiFi.h>
+#elif defined(ARDUINO_PORTENTA_H7_M7) || defined(ARDUINO_NICLA_VISION) || defined(ARDUINO_ARCH_ESP32) || defined(ARDUINO_GIGA) || defined(ARDUINO_OPTA)
+  #include <WiFi.h>
+#elif defined(ARDUINO_PORTENTA_C33)
+  #include <WiFiC3.h>
+#elif defined(ARDUINO_UNOR4_WIFI)
+  #include <WiFiS3.h>
+#endif
+
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = "111";    // your network SSID (name)
+char pass[] = "11111111";    // your network password (use for WPA, or use as key for WEP)
+
+// To connect with SSL/TLS:
+// 1) Change WiFiClient to WiFiSSLClient.
+// 2) Change port value from 1883 to 8883.
+// 3) Change broker value to a server with a known SSL/TLS root certificate 
+//    flashed in the WiFi module.
+
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
+
+const char broker[]    = "iot-06z00i8cm8497dy.mqtt.iothub.aliyuncs.com";
+int        port        = 1883;
+
+const char inTopic[]   = "/sys/k29156PnPTd/esp32/thing/service/property/set";
+const char outTopic[]  = "/sys/k29156PnPTd/esp32/thing/service/property/post";
+
+const long interval = 10000;
+unsigned long previousMillis = 0;
+
+int count = 0;
+
+void onMqttMessage(int messageSize);
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // attempt to connect to WiFi network:
+  Serial.print("Attempting to connect to WPA SSID: ");
+  Serial.println(ssid);
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    // failed, retry
+    Serial.print(".");
+    delay(5000);
+  }
+
+  Serial.println("You're connected to the network");
+  Serial.println();
+
+  mqttClient.setId("k29156PnPTd.esp32|securemode=2,signmethod=hmacsha256,timestamp=1737258555508|");
+  mqttClient.setUsernamePassword("esp32&k29156PnPTd", "390fa833ead87e1c8f719700282a3b41eb4ea6ed6d67a2bc16aa851d175d0ab4");
+
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+
+    while (1);
+  }
+
+  Serial.println("You're connected to the MQTT broker!");
+  Serial.println();
+
+  // set the message receive callback
+  mqttClient.onMessage(onMqttMessage);
+
+  Serial.print("Subscribing to topic: ");
+  Serial.println(inTopic);
+  Serial.println();
+
+  // subscribe to a topic
+  // the second parameter sets the QoS of the subscription,
+  // the the library supports subscribing at QoS 0, 1, or 2
+  int subscribeQos = 0;
+
+  mqttClient.subscribe(inTopic, subscribeQos);
+
+  // topics can be unsubscribed using:
+  // mqttClient.unsubscribe(inTopic);
+
+  Serial.print("Waiting for messages on topic: ");
+  Serial.println(inTopic);
+  Serial.println();
+}
+
+void loop() {
+  // call poll() regularly to allow the library to receive MQTT messages and
+  // send MQTT keep alives which avoids being disconnected by the broker
+  mqttClient.poll();
+
+  // to avoid having delays in loop, we'll use the strategy from BlinkWithoutDelay
+  // see: File -> Examples -> 02.Digital -> BlinkWithoutDelay for more info
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time a message was sent
+    previousMillis = currentMillis;
+
+    String payload;
+
+    payload += "hello world!";
+    payload += " ";
+    payload += count;
+
+    Serial.print("Sending message to topic: ");
+    Serial.println(outTopic);
+    Serial.println(payload);
+
+    // send message, the Print interface can be used to set the message contents
+    // in this case we know the size ahead of time, so the message payload can be streamed
+
+    bool retained = false;
+    int qos = 1;
+    bool dup = false;
+
+    mqttClient.beginMessage(outTopic, payload.length(), retained, qos, dup);
+    mqttClient.print(payload);
+    mqttClient.endMessage();
+
+    Serial.println();
+
+    count++;
+  }
+}
+
+void onMqttMessage(int messageSize) {
+  // we received a message, print out the topic and contents
+  Serial.print("Received a message with topic '");
+  Serial.print(mqttClient.messageTopic());
+  Serial.print("', duplicate = ");
+  Serial.print(mqttClient.messageDup() ? "true" : "false");
+  Serial.print(", QoS = ");
+  Serial.print(mqttClient.messageQoS());
+  Serial.print(", retained = ");
+  Serial.print(mqttClient.messageRetain() ? "true" : "false");
+  Serial.print("', length ");
+  Serial.print(messageSize);
+  Serial.println(" bytes:");
+
+  // use the Stream interface to print the contents
+  while (mqttClient.available()) {
+    Serial.print((char)mqttClient.read());
+  }
+  Serial.println();
+
+  Serial.println();
+}
